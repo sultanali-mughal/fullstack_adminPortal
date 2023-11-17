ca321e5be74dded2095b541aeb5e18dbb345369a
sultanali-mughal <sultanalimughal01@gmail.com>
2023-11-17 12:22:32 +0500

    add customers

--- server/controller/client.js
+++ server/controller/client.js
@@ -1,5 +1,7 @@
 import Product from '../models/product.js';
 import ProductStat from '../models/productStat.js';
+import User from '../models/user.js';
+
 
 export const getProducts = async (req, res) => {
     try {
@@ -22,3 +24,14 @@ export const getProducts = async (req, res) => {
       res.status(404).json({ message: error.message });
     }
   };
+
+  export const getCustomers = async (req, res) => {
+    try {
+      const customers = await User.find({role:"user"}). select('-password');
+  
+      res.status(200).json(customers);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+
+  }
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {getProducts} from '../controller/client.js';
+import {getProducts, getCustomers} from '../controller/client.js';
 
 
 const router = express.Router();
 
 router.get('/products', getProducts);
+router.get('/customers', getCustomers);
 
 export default router;
